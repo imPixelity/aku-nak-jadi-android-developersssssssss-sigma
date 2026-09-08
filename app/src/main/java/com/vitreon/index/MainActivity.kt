d9ba89216dbd6a6a7c7740e3f6c1a743e2cce312
phyxl <dzikrynp14@gmail.com>
2026-09-09 01:27:59 +0700

    y

--- app/src/main/java/com/vitreon/index/MainActivity.kt
+++ app/src/main/java/com/vitreon/index/MainActivity.kt
@@ -15,30 +15,48 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.vitreon.index.ui.theme.IndexTheme
+import kotlinx.coroutines.delay
+import kotlin.time.Duration.Companion.seconds
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            IndexTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    DisplayNathan(
+            MaterialTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = { ScaffoldTopBar() }
+                ) { innerPadding ->
+                    MainScreen(
                         modifier = Modifier.padding(innerPadding),
                         padding = innerPadding
                     )
@@ -49,23 +67,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IndexTheme {
-        Greeting("Android")
-    }
-}
-
-@Composable
-fun DisplayNathan(modifier: Modifier, padding: PaddingValues) {
+fun MainScreen(modifier: Modifier, padding: PaddingValues) {
     Column(
         modifier = modifier
     ) {
@@ -156,6 +158,78 @@ fun DisplayNathan(modifier: Modifier, padding: PaddingValues) {
                 ) {}
             }
 
+            Counter()
         }
+    }
+}
+
+@Composable
+fun Counter() {
+    var count by remember { mutableIntStateOf(1) }
+    val state = remember { mutableStateOf(0) }
+    val anotherState = remember(fun(): MutableState<Int> {
+        return mutableStateOf(0)
+    })
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Jumlah: $count")
+        Button(
+            onClick = { count *= 5 }
+        ) {
+            Text("Klik")
+        }
+
+        Text("Jumlah: ${state.value}")
+        Button(
+            onClick = { state.value++ }
+        ) {
+            Text("Klik")
+        }
+
+        LaunchedEffect(Unit) {
+            while (true) {
+                delay(1.seconds)
+                state.value++
+            }
+        }
+
+        Text("Jumlah: ${anotherState.value}")
+        Button(
+            onClick = { anotherState.value-- }
+        ) {
+            Text("Klik")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldTopBar() {
+    CenterAlignedTopAppBar(
+        title = { Text(text = "Counter", fontWeight = FontWeight.Bold) },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        ),
+    )
+}
+
+
+@Composable
+fun ScaffoldFloatingActionButton(
+    onClick: () -> Unit
+) {
+    FloatingActionButton(
+        contentColor = MaterialTheme.colorScheme.onPrimary,
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
+        onClick = onClick
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.add),
+            contentDescription = "Add+"
+        )
     }
 }
