22d9291fb4547c72c6dce8307db362dfbcc7ced2
phyxl <dzikrynp14@gmail.com>
2026-09-15 14:17:39 +0700

    ye

--- Quadrant/app/src/main/java/com/vitreon/quadrant/MainActivity.kt
+++ Quadrant/app/src/main/java/com/vitreon/quadrant/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.vitreon.quadrant.ui.theme.QuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +29,94 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QuadrantTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                QuadrantScreen()
             }
         }
     }
 }
 
+data class QuadrantData(
+    val color: Color,
+    val headerText: String,
+    val descText: String
+)
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun QuadrantScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier.weight(1f)
+        ) {
+            QuadrantCard(
+                modifier = Modifier.weight(1f),
+                quadrantData = QuadrantData(
+                    color = colorResource(id = R.color.q1),
+                    headerText = stringResource(id = R.string.q1_header),
+                    descText = stringResource(id = R.string.q1_desc)
+                )
+            )
+
+            QuadrantCard(
+                modifier = Modifier.weight(1f),
+                quadrantData = QuadrantData(
+                    color = colorResource(id = R.color.q2),
+                    headerText = stringResource(id = R.string.q2_header),
+                    descText = stringResource(id = R.string.q2_desc)
+                )
+            )
+        }
+
+        Row(
+            modifier = Modifier.weight(1f)
+        ) {
+            QuadrantCard(
+                modifier = Modifier.weight(1f),
+                quadrantData = QuadrantData(
+                    color = colorResource(id = R.color.q3),
+                    headerText = stringResource(id = R.string.q3_header),
+                    descText = stringResource(id = R.string.q3_desc)
+                )
+            )
+
+            QuadrantCard(
+                modifier = Modifier.weight(1f),
+                quadrantData = QuadrantData(
+                    color = colorResource(id = R.color.q4),
+                    headerText = stringResource(id = R.string.q4_header),
+                    descText = stringResource(id = R.string.q4_desc)
+                )
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    QuadrantTheme {
-        Greeting("Android")
+fun QuadrantCard(
+    modifier: Modifier = Modifier,
+    quadrantData: QuadrantData
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+            .background(quadrantData.color)
+            .padding(all = 16.dp)
+    ) {
+        Text(
+            text = quadrantData.headerText,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .fillMaxWidth()
+        )
+
+        Text(
+            text = quadrantData.descText,
+            textAlign = TextAlign.Justify
+        )
     }
 }
