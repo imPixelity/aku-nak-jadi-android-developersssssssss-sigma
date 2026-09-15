c16704d6026174f9121ba4beab01ee166347e588
phyxl <dzikrynp14@gmail.com>
2026-09-15 07:54:27 +0700

    ye

--- Article/app/src/main/java/com/vitreon/article/MainActivity.kt
+++ Article/app/src/main/java/com/vitreon/article/MainActivity.kt
@@ -1,29 +1,34 @@
 package com.vitreon.article
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.vitreon.article.ui.theme.ArticleTheme
 
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ArticleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold() { paddingValues ->
+                    ArticleScreen(modifier = Modifier.padding(paddingValues = paddingValues))
                 }
             }
         }
@@ -31,17 +36,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ArticleScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.bg_compose_background),
+            contentDescription = null
+        )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ArticleTheme {
-        Greeting("Android")
+        Text(
+            text = stringResource(id = R.string.first_text),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(all = 16.dp)
+        )
+
+        Text(
+            text = stringResource(id = R.string.second_text),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+
+        Text(
+            text = stringResource(id = R.string.third_text),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(all = 16.dp)
+        )
     }
 }
